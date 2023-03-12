d76c5e9e30cbb787a98aa3887048491fdf7e284d
Yuki_w6 <m1610600@gmail.com>
2023-03-12 23:02:21 +0900

    ocr test 2

--- ocr_test/test1.py
+++ ocr_test/test1.py
@@ -3,4 +3,10 @@ from PIL import Image
 
 str_img = Image.open('ocr_test/string.png')
 str = pytesseract.image_to_string(str_img)
+
+str_img2 = Image.open('ocr_test/string2.png')
+str2 = pytesseract.image_to_string(str_img2)
+
 print(str)
+
+print(str2)
